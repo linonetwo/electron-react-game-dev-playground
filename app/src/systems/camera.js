32b68d5ff24751365b5c1fc05ac0a068f49fc57f
linonetwo <linonetwo012@gmail.com>
2020-02-07 23:16:08 +0800

    lint: eslint

--- app/src/systems/camera.js
+++ app/src/systems/camera.js
@@ -1,26 +1,25 @@
-import { vAdd } from 'vec-la-fp';
-
+// @flow
 export default function camera({ entities, keysDown }) {
-  const camera = entities.find(e => e['@type'] === 'camera');
+  const cameraEntity = entities.find(e => e['@type'] === 'camera');
   const protagonistPawn = entities.find(e => e['@type'] === 'protagonistPawn');
-  if (camera) {
+  if (cameraEntity) {
     if (protagonistPawn) {
       // with protagonist, we center our view around the character
-      camera.x += Math.floor((protagonistPawn.x - camera.x) / 2);
-      camera.y += Math.floor((protagonistPawn.y - camera.y) / 2);
+      cameraEntity.x += Math.floor((protagonistPawn.x - cameraEntity.x) / 2);
+      cameraEntity.y += Math.floor((protagonistPawn.y - cameraEntity.y) / 2);
     } else {
       // without protagonist, we follow the arrow keys
       if (keysDown.includes('ArrowLeft')) {
-        camera.x -= 8;
+        cameraEntity.x -= 8;
       }
       if (keysDown.includes('ArrowRight')) {
-        camera.x += 8;
+        cameraEntity.x += 8;
       }
       if (keysDown.includes('ArrowUp')) {
-        camera.y -= 8;
+        cameraEntity.y -= 8;
       }
       if (keysDown.includes('ArrowDown')) {
-        camera.y += 8;
+        cameraEntity.y += 8;
       }
     }
   }
