ade02d4572bb1c5510c8121ec84e9d7d4e2ee115
linonetwo <linonetwo012@gmail.com>
2020-02-07 13:59:16 +0800

    faet: add a context menu

---
+++ app/src/components/ContextMenu/index.jsx
@@ -0,0 +1,164 @@
+// @flow
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import is from 'styled-is';
+import ReactDOM from 'react-dom';
+import { search } from 'fast-fuzzy';
+
+export type MenuItem = {
+  type: string,
+  icon?: string,
+};
+
+const menuHeight = 200;
+type MenuContainerProps = { top?: string, left?: string, opacity?: number };
+const MenuContainer = styled.div`
+  position: absolute;
+  top: ${({ top }: MenuContainerProps) => top || '-10000px'};
+  left: ${({ left }: MenuContainerProps) => left || '-10000px'};
+  z-index: 100;
+
+  /* margin between buttons */
+  display: flex;
+  flex-direction: column;
+
+  height: ${menuHeight}px;
+  padding: 8px 7px 6px;
+  background-color: white;
+  box-shadow: 0px 0px 8px 0px rgba(51, 51, 51, 0.6);
+  border-radius: 4px;
+  opacity: ${({ opacity }) => opacity || 0};
+  transition: opacity 0.25s;
+  overflow: scroll;
+
+  ::-webkit-scrollbar {
+    width: 0px; /* Remove scrollbar space */
+    background: transparent; /* Optional: just make scrollbar invisible */
+  }
+`;
+
+const MenuButton = styled.span`
+  cursor: pointer;
+
+  & .type {
+    color: #aaa;
+    font-weight: ${is('active')`800`};
+    font-size: 18px;
+    vertical-align: text-bottom;
+  }
+`;
+
+export type HoverMenuProps = {
+  open?: boolean,
+  mountPoint: string,
+  items: MenuItem[],
+  position: { x: ?number, y: ?number },
+};
+export default class HoverMenu extends Component<HoverMenuProps, *> {
+  state = {
+    filter: '',
+    position: { x: 0, y: 0 },
+  };
+
+  static getDerivedStateFromProps(nextProps: HoverMenuProps) {
+    // stop update menu position when it is open, so it won't follow cursor around
+    if (nextProps.open) {
+      return null;
+    }
+    return { position: nextProps.position };
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleSearch, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleSearch, false);
+  }
+
+  menuRef: HTMLDivElement | null = null;
+
+  /**
+   * When a mark button is clicked, toggle the current mark.
+   */
+  onClickMark = (item: MenuItem, event: SyntheticEvent<*>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  /**
+   * Render a mark-toggling toolbar button.
+   */
+  renderMarkButton = (item: MenuItem) => {
+    return (
+      <MenuButton
+        key={item.type}
+        onMouseDown={event => this.onClickMark(item, event)}
+      >
+        {item.type && <span className="type">{item.type}</span>}
+        {item.icon && <span className="icon">{item.icon}</span>}
+      </MenuButton>
+    );
+  };
+
+  getMenuStyle = () => {
+    if (this.menuRef === null) return {};
+    return {
+      opacity: 1,
+      top: `calc(${this.state.position.y}px)`,
+      left: `calc(${this.state.position.x}px - ${this.menuRef.offsetWidth /
+        2}px)`,
+    };
+  };
+
+  handleSearch = (event: KeyboardEvent) => {
+    const { opacity } = this.getMenuStyle();
+    if (opacity && this.props.items.length > 0) {
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.key === 'Escape') {
+        // 有时候用户按 Esc 其实会希望关闭选单
+        if (this.state.filter !== '') {
+          // 如果有字在过滤器里，才会是指清空过滤器
+          this.setState({ filter: '' });
+        }
+      } else if (event.key === 'Backspace' || event.key === 'Delete') {
+        this.setState(({ filter }) => ({
+          filter: filter.slice(0, Math.max(0, filter.length - 1)),
+        }));
+      } else if (/^[a-z0-9]$/.test(event.key)) {
+        this.setState(({ filter }) => ({ filter: filter + event.key }));
+      }
+    }
+  };
+
+  render() {
+    if (!this.props.open) return null;
+    const { opacity, top, left } = this.getMenuStyle();
+    const mountPoint = document.getElementById(this.props.mountPoint);
+    if (mountPoint && this.props.items.length > 0) {
+      const itemsToDisplay =
+        this.state.filter.length > 0
+          ? search(this.state.filter, this.props.items, {
+              keySelector: (item: MenuItem) => item.type,
+            })
+          : this.props.items;
+      return ReactDOM.createPortal(
+        <MenuContainer
+          data-usage="hover-menu"
+          opacity={opacity}
+          top={top}
+          left={left}
+          ref={elem => {
+            this.menuRef = elem;
+          }}
+        >
+          {this.state.filter}
+          {itemsToDisplay.map(this.renderMarkButton)}
+        </MenuContainer>,
+        mountPoint,
+      );
+    }
+    return null;
+  }
+}
--- app/src/pages/main/main.jsx
+++ app/src/pages/main/main.jsx
@@ -1,11 +1,12 @@
 // @flow
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { Stage } from 'react-pixi-fiber';
 import { changeMessage } from '../../redux/components/home/homeSlice';
 
 import HUD, { filterHUDEntities } from '../../components/HUD';
+import ContextMenu from '../../components/ContextMenu';
 import { initialSystems } from '../../systems';
 import { initialEntities } from '../../entites';
 import useGame from '../../useGame';
@@ -15,31 +16,55 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+const containerID = 'game-container';
 function Main(props) {
   const [entities, dispatchGameEvent] = useGame(
     initialSystems,
     initialEntities,
   );
+  const [contextMenuIsOpen, contextMenuIsOpenSetter] = useState(false);
+  const mousePosition = useRef({ x: 0, y: 0 });
 
   return (
-    <Container>
+    <Container id={containerID}>
       <HUD
         dispatchGameEvent={dispatchGameEvent}
         entities={filterHUDEntities(entities)}
       />
       <Stage
         onMouseMove={event => {
+          const position = { x: event.clientX, y: event.clientY };
+          // set
+          if (!contextMenuIsOpen) {
+            mousePosition.current = position;
+          }
           dispatchGameEvent({
             type: 'mouse-move',
-            x: event.clientX,
-            y: event.clientY,
+            ...position,
           });
         }}
-        options={{ backgroundColor: 0x10bb99, height: window.innerHeight, width: window.innerWidth }}
-        onContextMenu={event => event.preventDefault()}
+        options={{
+          backgroundColor: 0x10bb99,
+          height: window.innerHeight,
+          width: window.innerWidth,
+        }}
+        onContextMenu={event => {
+          event.preventDefault();
+          contextMenuIsOpenSetter(true);
+        }}
+        onClick={() => {
+          contextMenuIsOpenSetter(false);
+        }}
       >
         {entities}
       </Stage>
+
+      <ContextMenu
+        open={contextMenuIsOpen}
+        items={[{ type: 'aaa' }]}
+        position={mousePosition.current}
+        mountPoint={containerID}
+      />
     </Container>
   );
 }
